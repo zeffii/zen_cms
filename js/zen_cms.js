20f0241211e78c6c5ac0693a5b32e31c1543a208
Dealga McArdle <tetha.z@gmail.com>
2012-07-28 16:55:19 +0200

    shuffled the autoscroll to zen_cms

--- js/zen_cms.js
+++ js/zen_cms.js
@@ -54,4 +54,18 @@ $(document).ready(function() {
 
     }
 
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    }); 
+  
+    $('.scrollup').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 480);
+        return false;
+    });
+
 });
+
